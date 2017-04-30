54e4614c8d4a54850174d788f942189a24eb77cc
John Goodman <johncurtisgoodman@gmail.com>
2017-04-30 16:57:11 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,120 @@
+onload = function () {
+  var atlas;
+  atlas = document.getElementsByTagName('ATLAS')[0];
+  placePins();
+};
+
+var mapper = {
+  currentTerritory: '',
+  keyOpen: false,
+  lockCoords: null,
+  moving: false,
+  pinning: false,
+};
+
+var placePins = function () {
+  var map;
+  var i; var j;
+  map = JSON.parse(localStorage.getItem("map"));
+  console.log(JSON.stringify(map));
+  for (i = 0; i < map.allNames.length; i++) {
+    for (j = 0; j < map[map.allNames[i]].length; j++) {
+      console.log();
+      pin = document.createElement('PIN');
+      pin.style.left = map[map.allNames[i]][j].x;
+      pin.style.top = map[map.allNames[i]][j].y;
+      pin.coords = {
+        x: map[map.allNames[i]][j].x,
+        y: map[map.allNames[i]][j].y,
+      };
+      pin.onmouseenter = function () {
+        mapper.lockCoords = this.coords;
+        console.log(this.coords);
+      };
+      pin.onmouseleave = function () {
+        mapper.lockCoords = null;
+      };
+      document.body.appendChild(pin);
+    }
+  }
+};
+
+onkeydown = function (event) {
+  // Hold P or Z and click to place a new pin.
+  if (event.keyCode === 80 || event.keyCode === 90) {
+    mapper.pinning = true;
+  }
+  // Press M to open map key.
+  if (event.keyCode === 77) {
+    var mapKey = document.getElementById('map-key');
+    var territory = document.getElementById('territory-name');
+    var select = document.getElementById('submit-button');
+    if (mapper.keyOpen) {
+      if (document.activeElement !== territory) {
+        mapper.keyOpen = false;
+        mapKey.style.display = 'none';
+      }
+    } else {
+      map = JSON.parse(localStorage.getItem("map"));
+      mapper.keyOpen = true;
+      territory.value = mapper.currentTerritory;
+      mapKey.style.display = 'flex';
+    }
+    select.onclick = function () {
+      mapper.currentTerritory = territory.value;
+    };
+  }
+};
+
+onkeyup = function (event) {
+  mapper.pinning = false;
+};
+
+onclick = function (event) {
+  var map;
+  var pin;
+  var x;
+  var y;
+  if (mapper.pinning) {
+    map = JSON.parse(localStorage.getItem("map"));
+    console.log(map);
+    if (!map[mapper.currentTerritory]) {
+      map[mapper.currentTerritory] = [];
+    }
+    if (!map.allNames) {
+      map.allNames = [];
+    }
+    if (!map.allNames.includes(mapper.currentTerritory)) {
+      map.allNames.push(mapper.currentTerritory);
+    }
+    if (mapper.lockCoords) {
+      x = mapper.lockCoords.x;
+      y = mapper.lockCoords.y;
+    } else {
+      x = event.pageX;
+      y = event.pageY;
+    }
+    map[mapper.currentTerritory].push({
+      territory: mapper.currentTerritory,
+      number: map[mapper.currentTerritory].length,
+      x: x,
+      y: y,
+    });
+    pin = document.createElement('PIN');
+    pin.style.left = x;
+    pin.style.top = y;
+    pin.coords = {
+      x: x,
+      y: y,
+    };
+    pin.onmouseenter = function () {
+      mapper.lockCoords = this.coords;
+      console.log(this.coords);
+    };
+    pin.onmouseleave = function () {
+      mapper.lockCoords = null;
+    };
+    document.body.appendChild(pin);
+    localStorage.setItem("map", JSON.stringify(map));
+  }
+};
